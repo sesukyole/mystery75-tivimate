64b01e51914dac4fb70ee0e35cdddfbbbdd9dccb
mystery75 <152415726+mystery75@users.noreply.github.com>
2025-01-02 07:46:08 +0800

    Create MOVIES.js

---
+++ MOVIES.js
@@ -0,0 +1,107 @@
+[
+ {        
+  "name": "16 Puasa", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BZWUwY2RkNjUtYjdhMS00YTE5LWExODEtMGVhYTk4ZDdiOWVlXkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Kisah sekumpulan kawan baik di sebuah kampung yang ponteng puasa. Mereka menyembunyikan aktiviti mereka daripada pengetahuan isteri masing-masing. Mereka juga menetapkan jadual siapa yang akan membeli makanan di pekan dan kemudian makan di tempat-tempat terlindung. Berjayakah mereka berterusan sebegitu sepanjang bulan Ramadan?.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IDMVZ_IVP_LAND_917x516_2024221_204310.jpg", 
+           "director": ["Mamat Khalid"], 
+           "cast": ["Sabri Yunus,Aziz M. Osman,Bell Ngasri,Arab Khalid,Tj Isa,Sharwani Ns"],
+           "year": "2017"
+            }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/cbfc1b59-d17a-57e3-9a55-6b72c59ef9db/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "6a5580676416fb8b215e23638ddcd610:07ff72561ad9723e01be293effdd6b8d"
+ }, 
+ {        
+  "name": "17 Puasa", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMmI1YWIwNWUtYzYwYi00MjFiLWEzYjUtNTE3MzU0ZTUxOGE0XkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMmI1YWIwNWUtYzYwYi00MjFiLWEzYjUtNTE3MzU0ZTUxOGE0XkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Sebuah kisah komedi tentang enam orang sahabat yang tidak henti-henti menghadapi kesukaran berpuasa sepanjang Ramadan. Apabila takdir mempertemukan mereka, masalah mereka berlipat ganda.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IVIHW_IVP_LAND_917x516_378AF_20241130_41528.jpg", 
+           "director": ["Mamat Khalid"], 
+           "cast": ["Sabri Yunus,Aziz M. Osman,Bell Ngasri,Arab Khalid,Tj Isa,Sharwani Ns"],
+           "year": "2017"
+           }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/c5dfdbee-1bfb-59dc-bc04-047e7da111ef/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "edb936d4bce718c67da95c5958bc6010:917975e1fac313e63a89dc7680326d02"
+ }, 
+ {        
+  "name": "Lampir", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BY2IyNmQ0ZmEtNDFlNy00NmQ1LTlmZjQtZWJhOTdmZDQzYzFiXkEyXkFqcGc@._V1_FMjpg_UX390_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BY2IyNmQ0ZmEtNDFlNy00NmQ1LTlmZjQtZWJhOTdmZDQzYzFiXkEyXkFqcGc@._V1_FMjpg_UX390_.jpg", 
+           "plot": "Wendy dan Angga tengah sibuk mempersiapkan pernikahan mereka yang akan datang. Memutuskan untuk membuat kenangan indah sebelum hari besar mereka, mereka memilih sebuah vila megah bergaya vintage sebagai lokasi pemotretan mereka. Namun, kebahagiaan mereka seketika hilang ketika mereka mengetahui bahwa vila yang mereka tempati merupakan tempat keramat yang dikenal sebagai tempat persemayaman Mak Lampir, seorang makhluk gaib yang konon berniat untuk mendapatkan keabadian dan bernafsu menjadi manusia tercantik dengan cara yang mengerikan. Dalam upaya mereka untuk keluar dari vila angker tersebut, mereka dihadapkan dengan berbagai rintangan dan bahaya yang tidak terduga.", 
+           "backdrop": "https://cdns.klimg.com/resized/1200x600/p/headline/film-horor-lampir-segera-rilis-di-biosk-9a84a3.jpg", 
+           "director": ["Kenny Gulardi"], 
+           "cast": ["Jolene Marie,Rory Asyari,Gandhi Fernando"],
+           "year": "2024"
+           }, 
+           "video": "http://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/7b8e0a72-d310-56de-abd6-2597a53daa1c/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "fa86f27f7b2fccb5d5776551ada95810:4fe75f6fe382c606739ed0e86de7b84e"
+ }, 
+ {        
+  "name": "Underworld 2: Evolution", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMjEzNDY1OTQwOV5BMl5BanBnXkFtZTcwNjcxMTIzMw@@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMjEzNDY1OTQwOV5BMl5BanBnXkFtZTcwNjcxMTIzMw@@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Ketika perang antara Vampire dan Lycan terus marak, Selene, bekas ahli Death Dealers (unit pasukan khas vampire elit yang memburu werewolf) dan Michael, kacukan werewolf, bekerjasama sebagai usaha untuk merungkai rahsia-rahsia keturunan masing-masing.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_JCSVB_IVP_LAND_586x330_2024731_11341.jpg", 
+           "director": ["Len Wiseman"], 
+           "cast": ["Kate Beckinsale,Scott Speedman"],
+           "year": "2006"
+           }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/34775378-b3dd-5bfd-bde4-bbce61cffbb4/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "f91bb1d4913117a253cd19ddd5cb8d10:cf636f95a6f14641df7cb92825b4217a"
+ }, 
+ {        
+  "name": "Underworld 3: Rise of the Lycans", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMTAwNzYzMDQyMzReQTJeQWpwZ15BbWU3MDMyNDA0MjI@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMTAwNzYzMDQyMzReQTJeQWpwZ15BbWU3MDMyNDA0MjI@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Filem ini menjelaskan asal-usul permusuhan antara Vampire dan Lycan. Dibantu oleh kekasih gelapnya, Sonja, Lucian yang berani itu mengetuai Lycan dalam pertempuran menentang raja kejam Vampire, Victor. Berazam untuk menghentikan perhambaan raja itu terhadap puaknya, Lucian berdepan dengan tentera Death Dealer demi untuk mencapai kemerdekaan puak Lycan.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IAYZQ_IVP_LAND_586x330_2024731_103249.jpg", 
+           "director": ["Patrick Tatopoulos"], 
+           "cast": ["Rhona Mitra,Michael Sheen"],
+           "year": "2009"
+           }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/43b9a506-c72d-5f4b-874a-aa42b2ecf1ea/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "b0fef747d263b44b68f4ecd135233010:1e800b06018af545fea8eb3ea3342576"
+ }, 
+ {        
+  "name": "Underworld 4: Awakening", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMjAxMjc0ODk0OF5BMl5BanBnXkFtZTcwMTc5NDQwNw@@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMjAxMjc0ODk0OF5BMl5BanBnXkFtZTcwMTc5NDQwNw@@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Apabila kuasa manusia menemui kewujudan puak Vampire dan Lycan, perang untuk menghapuskan kedua-dua spesies bermula. Pahlawan vampire Selene mengetuai pertempuran melawan manusia.", 
+           "backdrop": "https://linear-poster.astro.com.my/prod/poster/IAYZP_IVP_LAND_917x516.jpg", 
+           "director": ["Bjorn Stein"], 
+           "cast": ["Kate Beckinsale, Michael Ealy"],
+           "year": "2012"
+           }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/d40e684a-15da-5d64-bf92-80f374320641/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "8993798bf2608c605557cf37e259c510:e12e81e172b585fa4eb8934a80c07885"
+ }, 
+ {        
+  "name": "Underworld 5: Blood Wars", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMjI5Njk0NTIyNV5BMl5BanBnXkFtZTgwNjU4MjY5MDI@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMjI5Njk0NTIyNV5BMl5BanBnXkFtZTgwNjU4MjY5MDI@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Selena dan Eve berpisah demi untuk melindungi Eve dari puak Lycan dan Vampire yang sedang memburu Selene. Seorang ketua Lycan baru, Marius, mengumpulkan tenteranya dan menyerang Eastern Coven. Selene telah direkrut dan kemudian dikhianati oleh Semira di Eastern Coven. Dia kemudian mengembara ke Nordic Coven untuk menerima kuasa yang diperlukannya untuk menamatkan perang antara Vampire dan Lycan.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IAYYZ_IVP_LAND_917x516_2024731_113437.jpg", 
+           "director": ["Anna Foerster"], 
+           "cast": ["Kate Beckinsale,Theo James"],
+           "year": "2016"
+           }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/bdc4d547-d631-5183-a593-77809d7f5b61/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "537ec5f8aec146f44e388f4161cd9110:37d368dfeba7ac1ae006467c08c4cae5"
+ }, 
+]  
+  
