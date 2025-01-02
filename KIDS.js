1f49cffaad1b8c454e04e1203f81f7cfb53bff38
mystery75 <152415726+mystery75@users.noreply.github.com>
2025-01-02 08:20:48 +0800

    Create KIDS.js

---
+++ KIDS.js
@@ -0,0 +1,130 @@
+[
+ {        
+  "name": "BoBoiBoy Movie", 
+  "category": "Kids", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BNmVlMDY3ZWItNmM3Ny00MjdhLWExNTQtZTQwZjM2NjA0NjgzXkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BNmVlMDY3ZWItNmM3Ny00MjdhLWExNTQtZTQwZjM2NjA0NjgzXkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "BoBoiBoy dan rakannya berhadapan dengan pemburu harta makhluk asing yang tamak sewaktu mereka bersaing menemukan objek purba yang berkuasa di sebuah pulau misteri.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_KBMAG_series_IVP_LAND_917x516_6A6D8_2024321_172815.jpg", 
+           "director": ["Nizam Abdul Razak"], 
+           "cast": [" Nur Sarah Alisya,  Anas Abdul Aziz,  Kevin Adrian Barnaby"],
+           "year": "2018"
+            }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/f00c164d-d7af-514a-9461-a76514e787c3/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "33f7fe50a6502992272d20748381b710:e1dd9fae5ef5b09323e99449dba08a01"
+ },  
+{        
+  "name": "BoBoiBoy Movie 2", 
+  "category": "Kids", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BYWNjMjY0ZjgtNjg0Zi00ZGNjLWJmM2MtZmYzZTU2NTU4OWM2XkEyXkFqcGc@._V1_QL75_UY562_CR21,0,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BYWNjMjY0ZjgtNjg0Zi00ZGNjLWJmM2MtZmYzZTU2NTU4OWM2XkEyXkFqcGc@._V1_QL75_UY562_CR21,0,380,562_.jpg", 
+           "plot": "BoBoiBoy dan rakan-rakannya menjadi sasaran penjahat kuno, Retak'ka yang inginkan kuasa semulajadinya dikembalikan semula agar dia boleh menjadi orang yang paling berkuasa di galaksi. BoBoiBoy dan gengnya harus mencari jalan untuk menewaskan Retak'ka dan menghalangnya dari merampas kuasa BoBoiBoy.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IWMJC_IVP_LAND_917x516_6A6D9_2024321_17291.jpg", 
+           "director": ["Nizam Abdul Razak"], 
+           "cast": ["Nur Fathiah Diaz,  Fadzli Mohd Rawi,  Dzubir Mohammed Zakaria"],
+           "year": "2019"
+            }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/e6ee43c8-212a-598d-88d9-f140f61f5a04/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "8f35c8959930b6df896b6b76a54d6b10:bfcdfc1adeb3b8c7d3723d08c9740701"
+ }, 
+ {        
+  "name": "Hotel Transylvania ", 
+  "category": "Kids", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMTM3NjQyODI3M15BMl5BanBnXkFtZTcwMDM4NjM0OA@@._V1_FMjpg_UX390_CR0,0,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMTM3NjQyODI3M15BMl5BanBnXkFtZTcwMDM4NjM0OA@@._V1_FMjpg_UX390_CR0,0,380,562_.jpg", 
+           "plot": "Selamat datang ke Hotel Transylvania, pusat peranginan mewah Dracula, di mana raksasa-raksasa dan keluarga mereka boleh berseronok dan tiada manusia dibenarkan. Suatu hujung minggu istimewa, Dracula telah mengajak semua teman baiknya untuk meraikan hari jadi ke-118 anak gadis kesayangan Mavis. Bagi Dracula untuk melayan semua raksasa legenda ini bukan satu masalah tetapi parti itu betul-betul bermula apabila seorang lelaki biasa terserempak dengan hotel itu dan merubah segalanya!", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_HNNYK_IVP_LAND_917x516_25A24_2024321_17951.jpg", 
+           "director": ["Genndy Tartakovsky"], 
+           "cast": ["Adam Sandler,Andy Samberg,Kevin James"],
+           "year": "2012"
+            }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/c1ca15f5-c39c-5d37-b029-c88a4e84e6ae/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "c74bc7c0b67e483d4021950d54977510:fb68f0e6e592b07b474ad8e77949d38d"
+ }, 
+ {        
+  "name": "Hotel Transylvania 2 ", 
+  "category": "Kids", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BY2ExYWJhMDYtYjcwMy00YjA2LTk2YzQtY2FhMWYxZThiNDQzXkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BY2ExYWJhMDYtYjcwMy00YjA2LTk2YzQtY2FhMWYxZThiNDQzXkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Hotel Transylvania kini terbuka kepada penginap-penginap manusia. Mavis dan Johnny mempunyai seorang anak lelaki bernama Dennis, yang kekurangan kebolehan pontianaknya membuat datuknya, Dracula, risau. Apabila Mavis dan Johnny pergi melawat ibubapa Johnny, Dracula memanggil kawan-kawannya untuk membantunya membuat Dennis jadi pontianak. Tetapi, satu lawatan yang tidak dijangka dari ayah Dracula yang konservatif dan membenci manusia iaitu Vlad, membuat keadaan menjadi tonggang-terbalik.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_HMOHK_IVP_LAND_917x516_7D81D_2024101_11831.jpg", 
+           "director": ["Genndy Tartakovsky"], 
+           "cast": ["Adam Sandler,Andy Samberg"],
+           "year": "2015"
+            }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/6d4adbf1-599f-564e-8f25-8fa79ad27939/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "20b0d86cf82587e42d1b11f1c5c15a10:bc1be7e9457b86cf2015cae747b3ba64"
+ },  
+ {        
+  "name": "Hotel Transylvania 3 ", 
+  "category": "Kids", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BNjA1MzU5MTY3OF5BMl5BanBnXkFtZTgwNTU5MDA3NTM@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BNjA1MzU5MTY3OF5BMl5BanBnXkFtZTgwNTU5MDA3NTM@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Pelayaran begitu lancar buat kawanan Dracula apabila kapal mewah raksasa menawarkan keseronokan dari bola tampar ke lawatan eksotik, dan bertemu sambil berjemur di bawah bulan. Namun, percutian impian bertukar menjadi mimpi ngeri apabila Mavis sedar Dracula telah jatuh hati dengan kapten kapal yang misteri, Ericka yang menyembunyikan rahsia berbahaya yang mungkin membinasakan semua raksasa.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IPBJU_100anniversary_IVP_LAND_917x516_2024819_12255.jpg", 
+           "director": ["Genndy Tartakovsky"], 
+           "cast": ["Adam Sandler,Andy Samberg,Selena Gomez"],
+           "year": "2018"
+            }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/8a8249b5-1045-5b07-b6a9-1a7da49b5bdb/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "8487d2e3654c546a057de86113466210:eb9feffab2c8d324fbda3b29a2b44ebf"
+ },  
+{        
+  "name": "How to Train Your Dragon", 
+  "category": "Kids", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_QL75_UX380_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_QL75_UX380_CR0,1,380,562_.jpg", 
+           "plot": "Set in the mythical world of burly Vikings and wild dragons, a Viking teenager's world is turned upside down when he is challenged by an unusual dragon.", 
+           "backdrop": "https://linear-poster.astro.com.my/prod/poster/10000151X864471_IVP_LAND_917x516.jpg", 
+           "director": ["CHRIS SANDERS"], 
+           "cast": ["CRAIG FERGUSON,GERARD BUTLER,JAY BARUCHEL"],
+           "year": "2010"
+            }, 
+           "video": "", 
+           "drm": "clearkey", 
+           "drmkey": ""
+ }, 
+{        
+  "name": "How to Train Your Dragon 2", 
+  "category": "Kids", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMzMwMTAwODczN15BMl5BanBnXkFtZTgwMDk2NDA4MTE@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMzMwMTAwODczN15BMl5BanBnXkFtZTgwMDk2NDA4MTE@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Lima tahun berlalu semenjak Hiccup dan Tootless menemukan naga-naga dengan Vikings Berk. Kini, mereka menghabiskan masa melakar kawasan di pulau itu. Ketika salah satu pengembaraan, kedua mereka menemui sebuah gua yang dihuni beratus-ratus naga liar dan 'Dragon Rider' misteri yang merupakan Valka, ibu Hiccup yang telah lama hilang. Hiccup dan Toothless kemudian mendapati diri mereka berdepan dengan pertarungan untuk melindungi Berk daripada perwira yang gilakan kuasa iaitu Drago.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_HSJGT_IVP_LAND_917x516_13E91_2024524_104436.jpg", 
+           "director": ["Dean DeBlois"], 
+           "cast": ["Jay Baruchel,Cate Blanchett"],
+           "year": "2014"
+            }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/e17ea0ea-f888-5824-9a19-41d1c2175fcc/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "020120bb1380f44b4e90b1f65d85ff10:fcabb2da2966c8086aa6b16aad5539ca"
+ }, 
+ {        
+  "name": "How to Train Your Dragon 3: The Hidden World", 
+  "category": "Kids", 
+  "info": { 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMjIwMDIwNjAyOF5BMl5BanBnXkFtZTgwNDE1MDc2NTM@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMjIwMDIwNjAyOF5BMl5BanBnXkFtZTgwNDE1MDc2NTM@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "plot": "Kini ketua dan pemerintah Berk bersama Astrid, Hiccup menghasilkan utopia naga penuh huru-hara. Apabila Light Fury betina tiba-tiba muncul serentak dengan ancaman paling buruk di kampung mereka, Hiccup dan Toothless terpaksa meninggalkan rumah dan mengembara ke dunia tersembunyi yang mereka rasa hanya wujud dalam mitos semata. Ketika takdir sebenar mereka diperlihatkan, naga dan penunggang berjuang bersama-sama hingga ke hujung Bumi untuk melindungi apa jua yang mereka usahakan untuk dihargai.", 
+           "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IVJIB_IVP_LAND_917x516_202431_102038.jpg", 
+           "director": ["Dean DeBlois"], 
+           "cast": ["Jay Baruchel,America Ferrera,F. Murray Abraham"],
+           "year": "2019"
+            }, 
+           "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/730a46c4-bfe1-5981-b53d-ebd60b24f567/default_primary.mpd", 
+           "drm": "clearkey", 
+           "drmkey": "6b7145b6c2ffa9585b8cfa2342670c10:c31162735079b18ff704c3281bef2004"
+ },  
+]
