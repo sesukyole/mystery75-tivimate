befa3608101f3c1ac5f1f3f8516a4dd445fe01db
mystery75 <152415726+mystery75@users.noreply.github.com>
2025-01-05 18:53:42 +0800

    Update VOD.js

--- VOD.js
+++ VOD.js
@@ -2,10 +2,9 @@
  {        
   "name": "16 Puasa", 
   "category": "Comedy",
-  "id": "Malay",
   "info": { 
-           "poster": "https://m.media-amazon.com/images/M/MV5BZWUwY2RkNjUtYjdhMS00YTE5LWExODEtMGVhYTk4ZDdiOWVlXkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
-           "bg": "https://m.media-amazon.com/images/M/MV5BZWUwY2RkNjUtYjdhMS00YTE5LWExODEtMGVhYTk4ZDdiOWVlXkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "poster": "https://m.media-amazon.com/images/M/MV5BZWUwY2RkNjUtYjdhMS00YTE5LWExODEtMGVhYTk4ZDdiOWVlXkEyXkFqcGc@._V1_QL100_UX600_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BZWUwY2RkNjUtYjdhMS00YTE5LWExODEtMGVhYTk4ZDdiOWVlXkEyXkFqcGc@._V1_QL100_UX600_.jpg", 
            "plot": "Kisah sekumpulan kawan baik di sebuah kampung yang ponteng puasa. Mereka menyembunyikan aktiviti mereka daripada pengetahuan isteri masing-masing. Mereka juga menetapkan jadual siapa yang akan membeli makanan di pekan dan kemudian makan di tempat-tempat terlindung. Berjayakah mereka berterusan sebegitu sepanjang bulan Ramadan?.", 
            "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IDMVZ_IVP_LAND_917x516_2024221_204310.jpg", 
            "director": ["Mamat Khalid"], 
@@ -21,8 +20,8 @@
   "name": "17 Puasa", 
   "category": "Comedy",
   "info": { 
-           "poster": "https://m.media-amazon.com/images/M/MV5BMmI1YWIwNWUtYzYwYi00MjFiLWEzYjUtNTE3MzU0ZTUxOGE0XkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
-           "bg": "https://m.media-amazon.com/images/M/MV5BMmI1YWIwNWUtYzYwYi00MjFiLWEzYjUtNTE3MzU0ZTUxOGE0XkEyXkFqcGc@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMmI1YWIwNWUtYzYwYi00MjFiLWEzYjUtNTE3MzU0ZTUxOGE0XkEyXkFqcGc@._V1_QL100_UX600_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMmI1YWIwNWUtYzYwYi00MjFiLWEzYjUtNTE3MzU0ZTUxOGE0XkEyXkFqcGc@._V1_QL100_UX600_.jpg", 
            "plot": "Sebuah kisah komedi tentang enam orang sahabat yang tidak henti-henti menghadapi kesukaran berpuasa sepanjang Ramadan. Apabila takdir mempertemukan mereka, masalah mereka berlipat ganda.", 
            "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IVIHW_IVP_LAND_917x516_378AF_20241130_41528.jpg", 
            "director": ["Mamat Khalid"], 
@@ -38,8 +37,8 @@
   "name": "300", 
   "category": "Action",
   "info": { 
-           "poster": "https://dai3fd1oh325y.cloudfront.net/images/480p/017484X0-portrait.jpg", 
-           "bg": "https://dai3fd1oh325y.cloudfront.net/images/480p/017484X0-portrait.jpg", 
+           "poster": "https://dai3fd1oh325y.cloudfront.net/images/720p/017484X0-portrait.jpg", 
+           "bg": "https://dai3fd1oh325y.cloudfront.net/images/720p/017484X0-portrait.jpg", 
            "plot": "Berdasarkan novel grafik paling laris oleh Frank Miller, 300 menceritakan kisah Raja Leonidas dan 300 pahlawan Sparta. Semasa tahun 400 sebelum Masihi, segerombolan tentera Parsi menuju ke Greece. Mereka menyediakan kata dua kepada penduduk Greece: berkhidmat kepada raja Parsi atau berdepan penghapusan habis-habisan. Sebuah kumpulan kecil 300 pejuang yang diketuai oleh raja Sparta berjaya melakukan sesuatu yang mustahil dan bangkit berdepan penindas dalam penghasilan kesan khas yang menarik ini.", 
            "backdrop": "https://dai3fd1oh325y.cloudfront.net/images/originald/017484X0-landscape.jpg", 
            "director": ["Zack Snyder"], 
@@ -55,8 +54,8 @@
   "name": "300: Rise Of An Empire", 
   "category": "Action",
   "info": { 
-           "poster": "https://dai3fd1oh325y.cloudfront.net/images/480p/017814X0-portrait.jpg", 
-           "bg": "https://dai3fd1oh325y.cloudfront.net/images/480p/017814X0-portrait.jpg", 
+           "poster": "https://dai3fd1oh325y.cloudfront.net/images/720p/017814X0-portrait.jpg", 
+           "bg": "https://dai3fd1oh325y.cloudfront.net/images/720p/017814X0-portrait.jpg", 
            "plot": "Berdasarkan novel grafik Frank Miller Xerxes, bab baru kisah epik ini memaparkan aksi di medan perang yang baru—di laut—sewaktu jeneral Themistokles cuba menyatukan Greece dengan mengetuai kuasa yang akan mengubah arah peperangan itu.", 
            "backdrop": "https://dai3fd1oh325y.cloudfront.net/images/originald/017814X0-landscape.jpg", 
            "director": ["Noam Murro"], 
@@ -72,7 +71,7 @@
   "name": "Lampir", 
   "category": "Horror",
   "info": { 
-           "poster": "https://m.media-amazon.com/images/M/MV5BY2IyNmQ0ZmEtNDFlNy00NmQ1LTlmZjQtZWJhOTdmZDQzYzFiXkEyXkFqcGc@._V1_FMjpg_UX390_.jpg", 
+           "poster": "https://m.media-amazon.com/images/M/MV5BY2IyNmQ0ZmEtNDFlNy00NmQ1LTlmZjQtZWJhOTdmZDQzYzFiXkEyXkFqcGc@._V1_QL100_UX600_.jpg", 
            "bg": "https://m.media-amazon.com/images/M/MV5BY2IyNmQ0ZmEtNDFlNy00NmQ1LTlmZjQtZWJhOTdmZDQzYzFiXkEyXkFqcGc@._V1_FMjpg_UX390_.jpg", 
            "plot": "Wendy dan Angga tengah sibuk mempersiapkan pernikahan mereka yang akan datang. Memutuskan untuk membuat kenangan indah sebelum hari besar mereka, mereka memilih sebuah vila megah bergaya vintage sebagai lokasi pemotretan mereka. Namun, kebahagiaan mereka seketika hilang ketika mereka mengetahui bahwa vila yang mereka tempati merupakan tempat keramat yang dikenal sebagai tempat persemayaman Mak Lampir, seorang makhluk gaib yang konon berniat untuk mendapatkan keabadian dan bernafsu menjadi manusia tercantik dengan cara yang mengerikan. Dalam upaya mereka untuk keluar dari vila angker tersebut, mereka dihadapkan dengan berbagai rintangan dan bahaya yang tidak terduga.", 
            "backdrop": "https://cdns.klimg.com/resized/1200x600/p/headline/film-horor-lampir-segera-rilis-di-biosk-9a84a3.jpg", 
@@ -88,8 +87,8 @@
   "name": "Underworld 2: Evolution", 
   "category": "Action",
   "info": { 
-           "poster": "https://m.media-amazon.com/images/M/MV5BMjEzNDY1OTQwOV5BMl5BanBnXkFtZTcwNjcxMTIzMw@@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
-           "bg": "https://m.media-amazon.com/images/M/MV5BMjEzNDY1OTQwOV5BMl5BanBnXkFtZTcwNjcxMTIzMw@@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMjEzNDY1OTQwOV5BMl5BanBnXkFtZTcwNjcxMTIzMw@@._V1_QL100_UX500_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMjEzNDY1OTQwOV5BMl5BanBnXkFtZTcwNjcxMTIzMw@@._V1_QL100_UX600_.jpg", 
            "plot": "Ketika perang antara Vampire dan Lycan terus marak, Selene, bekas ahli Death Dealers (unit pasukan khas vampire elit yang memburu werewolf) dan Michael, kacukan werewolf, bekerjasama sebagai usaha untuk merungkai rahsia-rahsia keturunan masing-masing.", 
            "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_JCSVB_IVP_LAND_586x330_2024731_11341.jpg", 
            "director": ["Len Wiseman"], 
@@ -104,8 +103,8 @@
   "name": "Underworld 3: Rise of the Lycans", 
   "category": "Action",
   "info": { 
-           "poster": "https://m.media-amazon.com/images/M/MV5BMTAwNzYzMDQyMzReQTJeQWpwZ15BbWU3MDMyNDA0MjI@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
-           "bg": "https://m.media-amazon.com/images/M/MV5BMTAwNzYzMDQyMzReQTJeQWpwZ15BbWU3MDMyNDA0MjI@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMTAwNzYzMDQyMzReQTJeQWpwZ15BbWU3MDMyNDA0MjI@._V1_QL100_UX500_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMTAwNzYzMDQyMzReQTJeQWpwZ15BbWU3MDMyNDA0MjI@._V1_QL100_UX500_.jpg", 
            "plot": "Filem ini menjelaskan asal-usul permusuhan antara Vampire dan Lycan. Dibantu oleh kekasih gelapnya, Sonja, Lucian yang berani itu mengetuai Lycan dalam pertempuran menentang raja kejam Vampire, Victor. Berazam untuk menghentikan perhambaan raja itu terhadap puaknya, Lucian berdepan dengan tentera Death Dealer demi untuk mencapai kemerdekaan puak Lycan.", 
            "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IAYZQ_IVP_LAND_586x330_2024731_103249.jpg", 
            "director": ["Patrick Tatopoulos"], 
@@ -120,8 +119,8 @@
   "name": "Underworld 4: Awakening", 
   "category": "Action",
   "info": { 
-           "poster": "https://m.media-amazon.com/images/M/MV5BMjAxMjc0ODk0OF5BMl5BanBnXkFtZTcwMTc5NDQwNw@@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
-           "bg": "https://m.media-amazon.com/images/M/MV5BMjAxMjc0ODk0OF5BMl5BanBnXkFtZTcwMTc5NDQwNw@@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMjAxMjc0ODk0OF5BMl5BanBnXkFtZTcwMTc5NDQwNw@@._V1_QL100_UX500_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMjAxMjc0ODk0OF5BMl5BanBnXkFtZTcwMTc5NDQwNw@@._V1_QL100_UX500_.jpg", 
            "plot": "Apabila kuasa manusia menemui kewujudan puak Vampire dan Lycan, perang untuk menghapuskan kedua-dua spesies bermula. Pahlawan vampire Selene mengetuai pertempuran melawan manusia.", 
            "backdrop": "https://linear-poster.astro.com.my/prod/poster/IAYZP_IVP_LAND_917x516.jpg", 
            "director": ["Bjorn Stein"], 
@@ -136,8 +135,8 @@
   "name": "Underworld 5: Blood Wars", 
   "category": "Action",
   "info": { 
-           "poster": "https://m.media-amazon.com/images/M/MV5BMjI5Njk0NTIyNV5BMl5BanBnXkFtZTgwNjU4MjY5MDI@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
-           "bg": "https://m.media-amazon.com/images/M/MV5BMjI5Njk0NTIyNV5BMl5BanBnXkFtZTgwNjU4MjY5MDI@._V1_FMjpg_UX390_CR0,1,380,562_.jpg", 
+           "poster": "https://m.media-amazon.com/images/M/MV5BMjI5Njk0NTIyNV5BMl5BanBnXkFtZTgwNjU4MjY5MDI@._V1_QL100_UX500_.jpg", 
+           "bg": "https://m.media-amazon.com/images/M/MV5BMjI5Njk0NTIyNV5BMl5BanBnXkFtZTgwNjU4MjY5MDI@._V1_QL100_UX500_.jpg", 
            "plot": "Selena dan Eve berpisah demi untuk melindungi Eve dari puak Lycan dan Vampire yang sedang memburu Selene. Seorang ketua Lycan baru, Marius, mengumpulkan tenteranya dan menyerang Eastern Coven. Selene telah direkrut dan kemudian dikhianati oleh Semira di Eastern Coven. Dia kemudian mengembara ke Nordic Coven untuk menerima kuasa yang diperlukannya untuk menamatkan perang antara Vampire dan Lycan.", 
            "backdrop": "https://vod-poster.astro.com.my/prod/IVP(STB_OTT_STV)_IAYYZ_IVP_LAND_917x516_2024731_113437.jpg", 
            "director": ["Anna Foerster"], 
