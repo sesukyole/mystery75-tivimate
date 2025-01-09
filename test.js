0b297a49e7ffd16e05dad36a9c8b6221fec465cb
mystery75 <152415726+mystery75@users.noreply.github.com>
2025-01-09 23:33:43 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,24 @@
+[
+  {
+    "name": "BoBoiBoy Galaxy Sori",
+    "category": "Kids",
+    "episodes": [       
+                  {
+                    "episode": 1,
+                    "name": "Kembali Beraksi!",
+                    "info": {
+                              "poster": "https://m.media-amazon.com/images/M/MV5BMzA0YmE3NjItZmNhYi00N2ViLWFhMzMtMThlYWU4Nzg2MTg0XkEyXkFqcGc@._V1_QL100_UX600_.jpg", 
+                              "bg": "https://m.media-amazon.com/images/M/MV5BMzA0YmE3NjItZmNhYi00N2ViLWFhMzMtMThlYWU4Nzg2MTg0XkEyXkFqcGc@._V1_QL100_UX600_.jpg", 
+                              "plot": "Selepas kehancuran Stesen TAPOPS, BoBoiBoy dan rakan-rakan pulang ke Bumi tetapi seorang musuh lama muncul kembali.",
+                              "backdrop": "https://m.media-amazon.com/images/M/MV5BMDE5MGI2MTYtNGFkOS00ODBiLWE1M2QtZGJmYjg5Mzc3MmRlXkEyXkFqcGc@._V1_QL100_UX950_.jpg", 
+                              "director": ["Nizam Razak"], 
+                              "cast": ["Nurfathiah Diaz","Nur Sarah Alisya Zainal Rashid","Yap Ee Jean","Dzubir Mohammed Zakaria"],
+                              "year": "2023"
+                            },
+                   "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/db51d0b1-5917-587f-9635-e4aa38754dee/default_primary.mpd", 
+                   "drm": "clearkey", 
+                   "drmkey": "e1b214ca2fc691d852cb4fe8f9a09210:d6b1d715831e982aa632f13756025e41"
+                 },  
+
+  }
+]
